d862c374682f5517e5cc736e90e86d76ff27b320
Timothy Karani <kartimothy@gmail.com>
2023-10-21 03:15:49 +0300

    updated next config

--- next.config.js
+++ next.config.js
@@ -10,6 +10,11 @@ const nextConfig = {
         hostname: "localhost",
         port: "8000",
       },
+      {
+        protocol: "https",
+        hostname:
+          "https://farm-bora-frontend.18rsgxmzc56t.eu-gb.codeengine.appdomain.cloud",
+      },
     ],
   },
   output: "standalone",
